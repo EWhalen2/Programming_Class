350460c6fea98a58846f751e0c743aa9040345e6
EWhalen2 <emmett.whalen@kellogg.northwestern.edu>
2023-01-24 01:12:23 +0000

    in class notes save 1

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -3,17 +3,53 @@
 # ruby 1-data.rb
 
 # In Ruby, there are different *types* of data:
+puts 5
+puts 2
 
 # Numbers
 
+
 # Perform simple math with numbers
+puts 5+2
+puts 5-2
+puts 5*2
+puts 7,6
+puts 9.0/4.0
+
+#order of operations
+
+puts 5 * 2 +1 
+puts 5 * (2 + 1)
 
 # Strings
+puts "Hello, world!"
+puts 'Hey World'
 
 # Combine strings together
+puts "Tacos are" + " yummy"
+puts "Tacos" * 3
 
+# puts "Tacos: " +3
+puts "Tacos: " +3.to_s
 # Variables
 
+food = "Tacos"
+quantity = 3
+puts food * quantity
+
 # Combine strings and variables
+first_name = " Boba"
+puts "hello," + first_name
+greeting = puts "Hello, #{first_name}!"
+puts greeting
+
+puts "tacos: {3}"
+puts "#{food}: #quantity}"
 
 # String manipulation
+puts "Hello". length
+puts "Hello". reverse
+
+puts "Hello".upcase
+puts "Hello".downcase
+puts "Hello".swapcase
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,63 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+is_true = true
+is_false = false
+
+#puts is_true
 
 # Boolean Expressions
+puts 3 < 2
+puts 3 == 2
+puts 3 != 2
 
 # If Conditional Logic
+if 3 > 2
+    puts "math works!"
+    puts "thank goodness!"
+end
+
+if  3 < 2
+    puts "whats????"
+     
+end
+
+if 3 > 2
+    puts "math works!"
+    puts "thank goodness!"
+else
+    puts "what???!"
+end
+
+if  3 < 2
+    puts "whats????"
+     
+end
+user_password = "tacos"
+submitted_password = "password1"
+if submitted_password == user_password
+    puts "you're logged in"
+else
+    puts "try again!"
+end
 
 # If/Else Conditional Logic
+team1_score = 101
+team2_score = 101
+if team1_score > team2_score
+    puts "team1 wins"
+elsif team2_score > team1_score
+    puts "team2 wins!"
+else
+    puts "it's a tie!"
+end
 
 # Elsif Conditional Logic
 
 # Combining Expressions
+temp = 68
+if  temp < 80 && temp > 60
+    puts "it's so nice"
+elsif temp >= 80 || temp <= 60
+    puts "Why!?!?" 
+end
